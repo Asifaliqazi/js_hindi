95bdcaa997db3516b57ddf70587aac762e00f5c5
Asifaliqazi <aali3015520@gmail.com>
2024-09-12 08:21:35 +0000

    container config text

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const customerID= 1;
+let customerName= "Asif ali";
+var customerEmail="aaa@gmail.com";
+customerNumber= 999;
+
+// const value don't change but let and var value are changable 
+
+console.table([customerID, customerName, customerEmail, customerNumber]);
+
+customerName="Ashu ali";
+customerEmail="bbb@gmail.com";
+customerNumber="888";
+
+console.table([customerID, customerName, customerEmail, customerNumber]);
