1334a83d717ac92c298d4366c91f332e62636f21
Asifaliqazi <aali3015520@gmail.com>
2024-09-18 11:24:30 +0000

    string methods

---
+++ .devcontainer/01_basics/04_methods of string.js
@@ -0,0 +1,11 @@
+/*
+typeof
+length
+toUpperCase()
+toLowerCase()
+replace("asif","ashu")
+string [numberof string];
+charAt(3)
+indexOf("f")
+substring(0, 4)
+*/
---
+++ .devcontainer/01_basics/04_string.js
@@ -0,0 +1,24 @@
+// const name="asif ali";
+// const repocount=50;
+// console.log(`hello my name is : ${name} and repoCount is : ${repocount}`);
+// const a=name.toUpperCase();
+// const b=name.toLowerCase();
+// const c=name[3];
+// const d=name.replace("asif","ashu");
+// const e=name.charAt(3);
+// const f=name.indexOf("f");
+// const g=name.substring(0, 4);
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// console.log(e);
+// console.log(f);
+// console.log(g);
+// console.log(h);
+
+const name="  asif ali     ";
+const ali=name.trim();
+console.log(ali);
+
+console.log(name);
