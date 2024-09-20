2d3667722a94b9002adf0f67af2904a3d29d2410
Asifaliqazi <aali3015520@gmail.com>
2024-09-20 11:30:13 +0000

    num and math and date

---
+++ .devcontainer/01_basics/05_nummath.js
@@ -0,0 +1,32 @@
+const bal=new Number(4000000);
+
+//number method toString()
+
+// const conbal=bal.toString();
+
+// console.log(typeof conbal);
+
+//number method toFixed(1,3,4) use in ecommerce website value after add 000
+
+// console.log(bal.toFixed(3));
+ 
+//number method toLocalString change comma to ruppes 
+
+// console.log(bal.toLocaleString("en-In"));
+
+
+// ''''''''''''''''*********************''''''''''''''''''''''''''''''
+
+/* Maths library*/
+
+// console.log(Math);
+// console.log(Math.abs (-4));
+// console.log(Math.round(4.5));
+// console.log(Math.min(4,2,5,8));
+// console.log(Math.max(4,2,5,8));
+
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10 +1));
+const min=20;
+const max=30;
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
---
+++ .devcontainer/01_basics/06_date.js
@@ -0,0 +1,8 @@
+// const myDate=new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+
+
+const myCreateDate=new Date("01-14-2024");
+console.log(myCreateDate.toLocalString());
