955bbbfa4682723a11a6aa5c7afc016bb1d61a15
Asifaliqazi <aali3015520@gmail.com>
2024-09-23 19:59:55 +0000

    array basic

---
+++ .devcontainer/01_basics/08_array.js
@@ -0,0 +1,13 @@
+const marvel=["ironman", "captain america", "spider man"]
+const dc=["superman", "batman", "greenlantern"]
+// const hero= marvel.concat(dc);
+// console.log(hero);
+// const hero=[...marvel, ...dc];
+// console.log(hero);
+console.log(Array.isArray("hitesh"));
+console.log(Array.from("hitesh"));
+
+    let a= "a";
+    let b= "a";
+    let c= "a";
+console.log(Array.of(a, b, c));
