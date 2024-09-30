ca11e53f57f5de00c52ec53f673ab44480c39026
Asifaliqazi <aali3015520@gmail.com>
2024-09-30 20:18:53 +0000

    loops

---
+++ .devcontainer/03_basics/02_loops.js
@@ -0,0 +1,49 @@
+// for(i=0; i<=10; i++){
+//     const element=i;
+//     if(element == 5){
+//         console.log("Number 5 is printed");
+//     }
+//     console.log(element);
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`this  is outer loop value ${i} `);
+//     for (let j = 1; j <= 10; j++) {
+//         console.log(`${i} * ${j} = ${i*j}`);
+        
+//     }
+    
+// }
+
+// const myarray=["rahul", "sham", "ganesh"];
+// console.log(myarray.length);
+// for (let index = 0; index < myarray.length; index++) {
+//     const element = myarray[index];
+//     console.log(element);
+// }
+
+
+//while loop
+
+// let i=2
+// while (i<=20) {
+//     console.log(`value of index is ${i}`);
+//     i= i+2
+// }
+
+//  let myarray=["rahul", "mahesh", "suresh"];
+//  arr=0
+// while (arr < myarray.length) {
+//     console.log(`value is ${myarray[arr]}`);
+//     arr= arr+1
+// }
+
+
+//do while loop
+let myarray=["rahul", "mahesh", "suresh"];
+arr=0;
+
+do {
+    console.log(`value is ${myarray[arr]}`)
+    arr= arr+1;
+} while (arr < myarray.length);
