af30782f397d9d5770983bf4cc9c1e1fad83c8f6
Asifaliqazi <aali3015520@gmail.com>
2024-09-30 19:37:35 +0000

    conditions

--- .devcontainer/01_basics/test.js
+++ .devcontainer/01_basics/test.js
@@ -1,1 +1,4 @@
-console.log("Hitesh")
+const accountname="ali asif";
+let accountemail="aa@gmail.com";
+var accountnumber323232;
+console.table([accountname, accountemail, accountnumber323232]);
---
+++ .devcontainer/03_basics/01_condition.js
@@ -0,0 +1,91 @@
+// const temperature=50;
+// if(temperature== 50){
+//     console.log(`temperatured, ${temperature} , is equal 50`);
+// }
+// else{
+//     console.log(`temperatured, ${temperature} , is less than 50`);
+// }
+
+// const score=200;
+// if(score>=190){
+//     var power="fly";
+//     console.log(`user power, ${power}`);
+// }
+// console.log(`user power, ${power}`);
+
+
+// const  balance=1000;
+// if(balance <= 500){
+//     console.log(`balance is greater than, ${balance}`);
+// }
+// else if(balance <= 800){
+//     console.log(`balance is greater than, ${balance}`);
+// }
+// else if(balance <= 1000){
+//     console.log(`balance is equal than, ${balance}`);
+// }
+// else{
+//     console.log("please enter correct value");
+// }
+
+// const userId=true;
+// const userPassword=false;
+// if (userId != userPassword ){
+//     console.log(`${userId}, is logged In`);
+// }
+// else{
+//     console.log(`${userId}, is not logged In`);
+
+// }
+
+
+// const month=4;
+// switch (month){
+//     case 1:
+//         console.log("jan");
+//         break;
+//     case 2:
+//         console.log("feb");
+//         break;
+//     case 3:
+//         console.log("March");
+//         break;
+//     default:
+//         console.log("this is not correct month")
+//         break;
+
+// }
+
+
+//falsy values
+// false
+// 0
+// -0
+// BigInt 0n
+// ""
+// null
+// undefined
+// NaN
+
+// truthy values
+// "0" 
+// 'false'
+// " "
+// []
+// {}
+// function(){}
+
+
+// const emobj={
+//     hitesh: "choudhary",
+//     choudhry: "Hitesh",
+//     Email: "hitesh123@gmail.com"
+// }
+// console.log(typeof Object.keys(emobj));
+
+
+//ternary operator example
+
+const result=500;
+result >= 100 ? console.log("A is greater") : console.log("A is not greater");
+
